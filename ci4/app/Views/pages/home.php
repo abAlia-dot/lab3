93548071c0a00eefac13f81bd8222ea253214745
abalia <abalia@student.apc.edu.ph>
2023-02-28 14:32:57 +0800

    Update Ci4

--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -1,1 +1,351 @@
-Welcome to my Homepage! Dave Alia
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Portfolio of Myself</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="img/Genshin_Ayaka_Icon.png" rel="icon">
+  <link href="img/Genshin_Ayaka_Icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+
+  <link href="<?=base_url('aos/aos.css');?>" rel="stylesheet">
+  <link href="<?=base_url('bootstrap/css/bootstrap.min.css');?>" rel="stylesheet">
+  <link href="<?=base_url('bootstrap-icons/bootstrap-icons.css');?>" rel="stylesheet">
+  <link href="<?=base_url('boxicons/css/boxicons.min.css');?>" rel="stylesheet">
+  <link href="<?=base_url('glightbox/css/glightbox.min.css');?>" rel="stylesheet">
+  <link href="<?=base_url('swiper/swiper-bundle.min.css');?>" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: iPortfolio - v3.10.0
+  * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="img/Dave_Profile.jpg" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.html">Aldrin Dave B Alia</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="https://en.wikipedia.org/wiki/Twitter" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="https://www.facebook.com/dave.alia.9" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="https://en.wikipedia.org/wiki/Instagram" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="https://en.wikipedia.org/wiki/Skype" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="https://www.linkedin.com/learning/?u=35279340" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+          <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+          <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
+          <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Aldrin Dave B Alia</h1>
+      <p><span class="typed" data-typed-items="RAM Student, Otaku, Gamer, Loner"></span></p>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>Hi! My name is Aldrin Dave B Alia you can all me Dave for short. </p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="img/Dave_Profile.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>UI/UX Designer &amp; Web Developer.</h3>
+            <p class="fst-italic">
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>27 Sept 2002</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>http://apcwebprog.csf.ph/~abalia</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>09956258031</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Pasay City</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>20</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>N/A</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>abalia@student.apc.edu.ph</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Student</span></li>
+                </ul>
+              </div>
+            </div>
+            <p>
+             An Introvert who just doesn't want to talk to other people. A person who stays at home and rarely go outside except when needed. My natural nature would be Wake Up, Eat, Study, Read, Watch Anime, Sleep and Repeat.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Portfolio</h2>
+          <p></p>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+              <li data-filter=".filter-app">Hobby</li>
+              <li data-filter=".filter-card">Games</li>
+              <li data-filter=".filter-web">Pictures</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/Hobby_1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="img/portfolio/Hobby_1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Reading Lightnovel and Manga"><i class="bx bx-plus"></i></a>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/Pictures_1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="img/portfolio/Pictures_1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Gala With Friends"><i class="bx bx-plus"></i></a>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/Hobby_3.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="img/portfolio/Hobby_3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Picturing with Cosplayers"><i class="bx bx-plus"></i></a>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/Games_1.jpeg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="img/portfolio/Games_1.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Playing Genshin Impact"><i class="bx bx-plus"></i></a>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/Pictures_2.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="img/portfolio/Pictures_2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Ako"><i class="bx bx-plus"></i></a>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/Hobby_2.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="img/portfolio/Hobby_2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Watching Anime"><i class="bx bx-plus"></i></a>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/Games_2.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="img/portfolio/Games_2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Playing Osu"><i class="bx bx-plus"></i></a>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/Games_3.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="img/portfolio/Games_3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Playing Muse Dash"><i class="bx bx-plus"></i></a>
+              
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="img/portfolio/Pictures_3.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="img/portfolio/Pictures_3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="With Others"><i class="bx bx-plus"></i></a>
+                
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+     <!-- ======= Contact Section ======= -->
+     <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Hi! feel free to contact me and please refrain from typing any profanity messages. Thank you</p>
+        </div>
+
+        <div class="row" data-aos="fade-in">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>28 Yakal St. Brgy Mulawin Malibay Pasay City</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>abalia@student.apc.edu.ph</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>9956258031</p>
+              </div>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2296.4757928308272!2d121.00908700700097!3d14.530748622893512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c949ddc22e23%3A0xd360ed5101e39c90!2sBarangay%20182%2C%20Mulawin%20Pasay%20City!5e0!3m2!1sen!2sph!4v1677383692533!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div>
+
+          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Full Name</label>
+                  <input type="text" name="full_name" class="form-control" id="name" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" name="subject_taken" id="subject" required>
+              </div>
+              <div class="form-group">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message_flow" rows="10" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Submit</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; <strong><span></span></strong>
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/ -->
+        <a href="https://bootstrapmade.com/"></a>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="purecounter/purecounter_vanilla.js"></script>
+  <script src="aos/aos.js"></script>
+  <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="glightbox/js/glightbox.min.js"></script>
+  <script src="isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="swiper/swiper-bundle.min.js"></script>
+  <script src="typed.js/typed.min.js"></script>
+  <script src="waypoints/noframework.waypoints.js"></script>
+  <script src="php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
+
+ 
+    
+</body>
+
+</html>
