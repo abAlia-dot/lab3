83880ee16352761513eb226a9cde4e337c43ab24
abalia <abalia@student.apc.edu.ph>
2023-03-02 11:44:51 +0800

    Update for Webprog

--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -5,26 +5,27 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
+
   <title>Portfolio of Myself</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="img/Ayaka.png" rel="icon">
-  <link href="img/Ayaka.png" rel="apple-touch-icon">
+  <link href="img/Genshin_Ayaka_Icon.png" rel="icon">
+  <link href="img/Genshin_Ayaka_Icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
 
-  <link href="<?=base_url('aos/aos.css');?>" rel="stylesheet">
-  <link href="<?=base_url('bootstrap/css/bootstrap.min.css');?>" rel="stylesheet">
-  <link href="<?=base_url('bootstrap-icons/bootstrap-icons.css');?>" rel="stylesheet">
-  <link href="<?=base_url('boxicons/css/boxicons.min.css');?>" rel="stylesheet">
-  <link href="<?=base_url('glightbox/css/glightbox.min.css');?>" rel="stylesheet">
-  <link href="<?=base_url('swiper/swiper-bundle.min.css');?>" rel="stylesheet">
 
+  <link href="<?= base_url('aos/aos.css'); ?>"  rel="stylesheet">
+  <link href="<?= base_url('bootstrap/css/bootstrap.min.css'); ?>"  rel="stylesheet">
+  <link href="<?= base_url('bootstrap-icons/bootstrap-icons.css'); ?>"  rel="stylesheet">
+  <link href="<?= base_url('boxicons/css/boxicons.min.css'); ?>"  rel="stylesheet">
+  <link href="<?= base_url('glightbox/css/glightbox.min.css'); ?>"  rel="stylesheet">
+  <link href="<?= base_url('swiper/swiper-bundle.min.css'); ?>"  rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="css/style.css" rel="stylesheet">
