44661b434332203715e424113a0c10f590b14838
abalia <abalia@student.apc.edu.ph>
2023-03-02 12:56:41 +0800

    Update Again

--- ci4/app/Views/templates/footer.php
+++ ci4/app/Views/templates/footer.php
@@ -1,18 +1,5 @@
 <em>&copy; 2021</em>
 
- <!-- Vendor JS Files -->
- <script src="purecounter/purecounter_vanilla.js"></script>
-  <script src="aos/aos.js'); ?>"></script>
-  <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="glightbox/js/glightbox.min.js"></script>
-  <script src="isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="swiper/swiper-bundle.min.js"></script>
-  <script src="typed.js/typed.min.js"></script>
-  <script src="waypoints/noframework.waypoints.js"></script>
-  <script src="php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="js/main.js"></script>
-
+<?php echo script_tag('js/main.js'); ?>
 </body>
 </html>
